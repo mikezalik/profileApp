c5a30549f1a6fcf986c9c7b8ac4d998ee5cf1c98
mikezalik <mike.zalik@gmail.com>
2018-11-12 22:34:20 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//TODO: connect to API URL (https://teamtreehouse.com/michaelzalik.json)
+//TODO: read the data
+//TODO: parse data
+//TODO: print data
+
+//print message to console
+function printMessage(username, badgeCount, points){
+     const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+    console.log(message);
+    }
+
+printMessage("Mike", 2, 400);
