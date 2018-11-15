8d46ee4ae05465b8fb21263f52a3ebc29c532dd6
mikezalik <mike.zalik@gmail.com>
2018-11-14 22:21:03 -0800

    JS

--- app.js
+++ app.js
@@ -5,8 +5,6 @@
 const https = require('https');
 const username = "michaelzalik";
 
-
-
 //print message to console
 function printMessage(username, badgeCount, points){
      const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
