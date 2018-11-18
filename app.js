d9fe87634b82f394c9fdf5a39ff6baf13d2d2d11
mikezalik <mike.zalik@gmail.com>
2018-11-18 12:03:45 -0800

    JS

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //command line based node app
 //TODO: code similar functionality for another API?
 
+//require profile.js
 const profile = require('./profile.js');
 
+//username input
 const users = process.argv.slice(2);
 users.forEach(profile.get);
 
