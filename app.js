eeb5b4c52cf05c10cc3857ffdbfd1c7e14f5c8f7
mikezalik <mike.zalik@gmail.com>
2018-11-18 11:52:24 -0800

    JS

--- app.js
+++ app.js
@@ -1,57 +1,9 @@
 //command line based node app
 //TODO: code similar functionality for another API?
 
-//require https module
-const https = require('https');
-//require http module
-const http = require('http');
-const username = "michaelzalik";
-
-//print error messages
-function printError(error){
-    console.error(error.message);
-}
-
-//print message to console
-function printMessage(username, badgeCount, points){
-     const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
-    console.log(message);
-    }
-    
-function getProfile(username) {
-    try {
-//connect to API URL (https://teamtreehouse.com/${username}.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-    if (response.statusCode === 200) {
-    let body = "";
-//read data
-        response.on("data", data => {
-            body += data.toString();
-        });
-//parse data
-        
-        response.on('end', () => {
-            try {
-            const profile = JSON.parse(body);
-//print data
-            printMessage(username, profile.badges.length, profile.points.JavaScript);
-         } catch (error) {
-             printError(error);
-         } });
-        } else {
-            const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
-            const statusCodeError = new Error(message);
-            printError(statusCodeError);
-        }
-});
-
-request.on('error', printError);
-} catch (error) {
-        printError(error);
-        }
-    }
+const profile = require('./profile.js');
 
 const users = process.argv.slice(2);
-users.forEach(getProfile);
+users.forEach(profile.get);
 
  
