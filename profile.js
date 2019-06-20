45dd9f0c0a73466013753f04b83d654be680ec18
mikezalik <mike.zalik@gmail.com>
2019-06-19 20:35:33 -0700

    profile js refactor

--- profile.js
+++ profile.js
@@ -1,55 +1,57 @@
-//command line based node app
-//TODO: Code similar functionality for another API?
+// command line based node app
+// TODO: Code similar functionality for another API?
 
-//require https module
+// require https module
 const https = require('https');
 
-//require http module
+// require http module
 const http = require('http');
-const username = "michaelzalik";
 
-//print error messages
-function printError(error){
-    console.error(error.message);
+const username = 'michaelzalik';
+
+// print error messages
+function printError(error) {
+  console.error(error.message);
 }
 
-//print message to console
+// print message to console
 function printMessage(username, badgeCount, points) {
-    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
-        console.log(message);
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+  console.log(message);
 }
-    
-function get(username) {
-    try {
-//connect to API URL (https://teamtreehouse.com/${username}.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-    if (response.statusCode === 200) {
-    let body = "";
-//read data
-        response.on("data", data => {
-            body += data.toString();
-        });
-//parse data
-        
-        response.on('end', () => {
-            try {
-        const profile = JSON.parse(body);
-//print data
-        printMessage(username, profile.badges.length, profile.points.JavaScript);
-            } catch (error) {
-        printError(error);
-    }});
-        } else {
-            const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
-            const statusCodeError = new Error(message);
-            printError(statusCodeError);
-        }
-});
 
-request.on('error', printError);
-    } catch (error) {
+function get(username) {
+  try {
+    // connect to API URL (https://teamtreehouse.com/${username}.json)
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) => {
+      if (response.statusCode === 200) {
+        let body = '';
+        // read data
+        response.on('data', (data) => {
+          body += data.toString();
+        });
+        // parse data
+
+        response.on('end', () => {
+          try {
+            const profile = JSON.parse(body);
+            // print data
+            printMessage(username, profile.badges.length, profile.points.JavaScript);
+          } catch (error) {
             printError(error);
-        }
+          }
+        });
+      } else {
+        const message = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+        const statusCodeError = new Error(message);
+        printError(statusCodeError);
+      }
+    });
+
+    request.on('error', printError);
+  } catch (error) {
+    printError(error);
+  }
 }
 
 module.exports.get = get;
